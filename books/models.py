856a77b905870a0864b4cf93a257a2d40fcfe148
FMutale1108 <francisyotam8@gmail.com>
2024-11-29 01:51:10 +0300

    Add files via upload

--- books/models.py
+++ books/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    read_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
--- books/views.py
+++ books/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Filter by genre or read_status if provided in query parameters
+        genre = self.request.query_params.get('genre')
+        read_status = self.request.query_params.get('read_status')
+
+        if genre:
+            queryset = queryset.filter(genre__icontains=genre)
+        if read_status:
+            queryset = queryset.filter(read_status=read_status.lower() == 'true')
+
+        return queryset
+
+
+class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
